2e49743493782089adcd6b4207178037b562704c
matteogav <matteogav@gmail.com>
2019-11-11 10:21:49 +0100

    1er commit

--- phone.cpp
+++ phone.cpp
@@ -1,1 +1,55 @@
 #include "phone.hpp"
+
+phone(nat num=0, const string& name="", nat compt=0) throw(error){
+
+}
+
+/* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
+phone(const phone& T) throw(error){
+
+}
+phone& operator=(const phone& T) throw(error){
+
+}
+~phone() throw(){
+
+}
+
+nat numero() const throw(){
+
+}
+
+string nom() const throw(){
+
+}
+
+nat frequencia() const throw(){
+
+}
+
+phone& operator++() throw(){
+
+}
+
+phone operator++(int) throw(){
+
+}
+
+bool operator==(const phone& T) const throw(){
+
+}
+bool operator!=(const phone& T) const throw(){
+
+}
+bool operator<(const phone& T) const throw(){
+
+}
+bool operator>(const phone& T) const throw(){
+
+}
+bool operator<=(const phone& T) const throw(){
+
+}
+bool operator>=(const phone& T) const throw(){
+
+}
