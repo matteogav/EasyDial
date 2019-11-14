685a415f612dbc393272f145781c53e79c70c455
pol <stopenheaven@gmail.com>
2019-11-14 12:28:59 +0100

    en teoria tot va bé, per comprovar: primer fer make prova_phone i executar el. phone_prova

--- Makefile
+++ Makefile
@@ -5,11 +5,16 @@ opcions= -std=c++98 -g -O0 -ansi -Wall
 #compilar: g++ -c -Wall arxiu.cpp
 #montar: g++ -o nom_executable.e arxiu1.o arxiu2.o -lesin
 
-TARGETS  = phone call_registry easy_dial dialog	monta clean cleano
+TARGETS  = phone call_registry easy_dial dialog	monta clean cleano prova_phone
 all: $(TARGETS)
 
 phone:
 	$(CC) $(opcions) phone.cpp
+
+prova_phone:
+	$(CC) $(opcions) phone.cpp
+	$(CC) $(opcions) phone_prova.cpp
+	$(CO) phone_prova phone_prova.o phone.o -lesin
 
 call_registry:
 	$(CC) $(opcions) call_registry.cpp
--- phone.cpp
+++ phone.cpp
@@ -1,7 +1,8 @@
 #include "phone.hpp"
 
+//buscar en name si hi ha un dels caracters "prohibits"
 phone::phone(nat num, const string& name, nat compt) throw(error){
-  size_t busca = name.find(DELETECHAR); 
+  size_t busca = name.find(DELETECHAR);
   if (busca != string::npos) throw(ErrNomIncorrecte);
 
   busca = name.find(ENDCHAR); 
@@ -9,63 +10,81 @@ phone::phone(nat num, const string& name, nat compt) throw(error){
 
   busca = name.find(ENDPREF); 
   if (busca != string::npos) throw(ErrNomIncorrecte);
+  telf=num;
+  contacte=name;
+  freq=compt;
 }
 
 /* Tres grans. Constructor per còpia, operador d'assignació i destructor. */
 phone::phone(const phone& T) throw(error){
-  num = T.num;
-  name = T.name;
-  compt = T.compt;
-
+  telf = T.telf;
+  contacte = T.contacte;
+  freq = T.freq;
 }
 phone& phone::operator=(const phone& T) throw(error){
-  num = T.num;
-  name = T.name;
-  compt = T.compt;
+  telf = T.telf;
+  contacte = T.contacte;
+  freq = T.freq;
   return *this;
 }
-phone::~phone() throw(){
-
-}
+phone::~phone() throw(){}
 
 nat phone::numero() const throw(){
-  return num;
+  return telf;
 }
 
 string phone::nom() const throw(){
-  return name;
+  return contacte;
 }
 
 nat phone::frequencia() const throw(){
-  return compt;
+  return freq;
 }
 
 phone& phone::operator++() throw(){
-  ++compt;
+  ++freq;
   return *this;
 }
 
 phone phone::operator++(int) throw(){
   phone p = *this;
-  ++compt;
+  ++freq;
   return p;
 }
 
 bool phone::operator==(const phone& T) const throw(){
-  return (num == T.num and name == T.name and compt == T.compt);
+  return (telf == T.telf and contacte == T.contacte and freq == T.freq);
 }
 bool phone::operator!=(const phone& T) const throw(){
   return !(*this == T);
 }
 bool phone::operator<(const phone& T) const throw(){
-  return (compt < T.compt and name < T.name);
+  bool res;
+  if (freq < T.freq) res=true;
+  else if (freq == T.freq){
+    if (contacte < T.contacte) res=true;
+    else res=false;
+  }
+  else res=false;
+
+  return res;
+  //return (freq < T.freq and contacte < T.contacte);
 }
 bool phone::operator>(const phone& T) const throw(){
-  return (compt > T.compt and name > T.name);
+  bool res;
+  if (freq > T.freq) res=true;
+  else if (freq == T.freq){
+    if (contacte > T.contacte) res=true;
+    else res=false;
+  }
+  else res=false;
+
+  return res;
+  //return (freq > T.freq and contacte > T.contacte);
 }
 bool phone::operator<=(const phone& T) const throw(){
-  return (compt <= T.compt and name <= T.name);
+  return (freq <= T.freq and contacte <= T.contacte);
 }
 bool phone::operator>=(const phone& T) const throw(){
-  return (compt >= T.compt and name >= T.name);
+  return (freq >= T.freq and contacte >= T.contacte);
 }
