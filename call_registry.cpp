abc4f7876f0074d55b7f167cd7df6852b6c5ebf7
matteogav <matteogav@gmail.com>
2019-11-25 10:07:42 +0100

    Popop

--- call_registry.cpp
+++ call_registry.cpp
@@ -20,7 +20,7 @@ void call_registry::afegir(phone p){
   node_hash* aux = _taula[i];
   bool trobat = false;
   while(aux != NULL and not trobat){
-    if(aux->_phone.numero() == num) trobat = true;
+    if(aux->_phone.numero() == p.numero()) trobat = true;
     else aux = aux->_seg;
   }
 
@@ -35,23 +35,23 @@ call_registry::call_registry() throw(error){
 	_mida = 10;
   _n_elements = 0;
 	_taula = new node_hash*[_mida];
-  for(int i=0; i<_mida ; ++i) _taula[i] = NULL;
+  for(nat i=0; i<_mida ; ++i) _taula[i] = NULL;
 }
 
 /* Constructor per còpia, operador d'assignació i destructor. */
 call_registry::call_registry(const call_registry& R) throw(error){
 
-  _mida = R->_mida;
+  _mida = R._mida;
   _n_elements = _n_elements;
-  /*for(int i=0; i < R->_mida; ++i){
-    _taula[i] = R->_taula[i];
-  }*/
+  for(nat i=0; i < R._mida; ++i){
+    _taula[i] = R._taula[i];
+  }
 }
 call_registry& call_registry::operator=(const call_registry& R) throw(error){
-  if(this != R){
+  if(this != &R){
     _mida = R._mida;
     _n_elements = _n_elements;
-    for(int i=0; i < R._mida; ++i){
+    for(nat i=0; i < R._mida; ++i){
       _taula[i] = R._taula[i];
     }
   }
@@ -80,8 +80,8 @@ void call_registry::assigna_nom(nat num, const string& name) throw(error){
   node_hash* aux = _taula[i];
 
 	if (aux->_phone.numero() == num){
-		phone modificat(num, name, (_taula[i]._phone).frequencia());
-		(_taula[i]._phone) = modificat;
+		phone modificat(num, name, (aux->_phone).frequencia());
+		(aux->_phone) = modificat;
 	}
 	else {
 		phone nou(num, name, 0);
@@ -150,6 +150,8 @@ string call_registry::nom(nat num) const throw(error){
 
 nat call_registry::num_trucades(nat num) const throw(error){
 	nat i = hash(num);
+	node_hash* aux = _taula[i];
+	bool b = false;
   nat res;
 
   while(aux != NULL and not b){
