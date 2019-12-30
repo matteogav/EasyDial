553576be91ed39ebc69c0f44dc932ac4fe6b7da7
matteogav <45036430+matteogav@users.noreply.github.com>
2019-12-30 14:39:04 +0100

    node_pare fet

--- easy_dial.cpp
+++ easy_dial.cpp
@@ -8,37 +8,40 @@ void easy_dial::insereix(const phone& P, bool &arrel){
   string ant = "";
   string nom = P.nom();
   nat tlf = P.numero();
-  _arrel = rinsereix(_arrel, i, s, nom, tlf, arrel, ant);
+  _arrel = rinsereix(_arrel, i, s, nom, tlf, arrel, ant, _arrel);
   //cout << "ARREL_AF:" << _arrel->antFr<<endl;
 }
 
-easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string &nom, const nat &telef, bool &arrel, string ant_ant) throw(error){
+easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string &nom, const nat &telef, bool &arrel, string ant_ant, node_tst* res) throw(error){
   if(n==NULL){
-    node_tst* n = new node_tst;
-    n->_esq = n->_dret = n->_central = NULL;
-    n->_lletra = s[i];
+    node_tst* m = new node_tst;
+    m->_esq = m->_dret = m->_central = NULL;
+    m->_lletra = s[i];
     try {
       if(arrel){
-        n->maxFr = ant_ant;
-        n->antFr = nom;
-        n->_tlf = telef;
+        m->maxFr = ant_ant;
+        m->antFr = nom;
+        m->_tlf = telef;
+        m->_pare = NULL;
         arrel = false;
         i++;
-        n->_central = rinsereix(n->_central, i, s, ant_ant, telef, arrel, ant_ant);
+        m->_central = rinsereix(m->_central, i, s, ant_ant, telef, arrel, ant_ant, m);
         /*cout<<"r_inser_arrel_nom:"<<nom<<endl;
 				cout<<"r_inser_arrel_mF:"<<n->maxFr<< "."<<endl;
 				cout<<"r_inser_arrel_aF:"<<n->antFr<< "."<<endl;*/
       }
       else{
         if (i < s.length()) {
-          n->maxFr = nom;
-          n->antFr = ant_ant;
-          n->_tlf = telef;
-          ant_ant = n->maxFr;
-          //n->_freq = frequen;      //CAL???
+          m->maxFr = nom;
+          m->antFr = ant_ant;
+          m->_tlf = telef;
+          ant_ant = m->maxFr;
+					m->_pare = n;
           nom = "";
           i++;
-          n->_central = rinsereix(n->_central, i, s, nom, telef, arrel, ant_ant);
+          //cout<<"el pare de "<<m->_lletra <<"es:";
+					//if (res!=NULL) cout<<res->_lletra<<" ."<<endl;
+          m->_central = rinsereix(m->_central, i, s, nom, telef, arrel, ant_ant, m);
           /*cout<<"r_inser_nom:"<<nom<<endl;
 					cout<<"r_inser_mF:"<<n->maxFr<< "."<<endl;
 					cout<<"r_inser_aF:"<<n->antFr<< "."<<endl;*/
@@ -46,14 +49,14 @@ easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string 
       }
     }
     catch (error) {
-      delete n;
+      delete m;
       throw;
     }
-    return n;
+    return m;
   }
   else{
-    if (n->_lletra > s[i]) n->_esq = rinsereix(n->_esq, i, s, nom, telef, arrel, n->antFr);
-    else if (n->_lletra < s[i]) n->_dret = rinsereix(n->_dret, i, s, nom, telef, arrel, n->antFr);		
+    if (n->_lletra > s[i]) n->_esq = rinsereix(n->_esq, i, s, nom, telef, arrel, n->antFr, n);
+    else if (n->_lletra < s[i]) n->_dret = rinsereix(n->_dret, i, s, nom, telef, arrel, n->antFr, n);		
     else {
       if(n->maxFr == ""){
         n->maxFr = nom;
@@ -62,7 +65,7 @@ easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string 
       }
       ant_ant = n->maxFr;
       i++;
-      n->_central = rinsereix(n->_central, i, s, nom, telef, arrel, ant_ant);     // (n->_lletra == s[i])
+      n->_central = rinsereix(n->_central, i, s, nom, telef, arrel, ant_ant, n);     // (n->_lletra == s[i])
     }
     return n;
   }
@@ -185,16 +188,12 @@ easy_dial::easy_dial(const call_registry& R) throw(error){
   bool arr = true;
   _arrel = NULL;
 
-  cout<<"v.size() "<<v.size()<<endl;
-
   if(v.size() > 0){
     //ordenar vector per frequencia mes alta a mes baixa, he triat mergesort
     ordena(v,v.size());
 
     _nom_arrel = v[0].nom();
-		for(unsigned int i = 0; i<v.size(); ++i){
-			 cout <<"vector["<<i<< "]: "<<(v[i].nom())<<endl;
-    }
+		//for(unsigned int i = 0; i<v.size(); ++i) cout <<"vector["<<i<< "]: "<<(v[i].nom())<<endl;
     for(unsigned int i = 0; i<v.size(); ++i){
       //cout <<"vector: "<<(v[i].nom())<<endl;
 		 insereix(v[i], arr);
