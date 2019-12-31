e22511da5eeeaefca43f23ea255c97365de2e086
pol <stopenheaven@gmail.com>
2019-12-31 17:24:04 +0100

    FUNCIONA easy_dial

--- easy_dial.cpp
+++ easy_dial.cpp
@@ -8,11 +8,15 @@ void easy_dial::insereix(const phone& P, bool &arrel){
   string ant = "";
   string nom = P.nom();
   nat tlf = P.numero();
-  _arrel = rinsereix(_arrel, i, s, nom, tlf, arrel, ant, _arrel);
+  nat freq = P.frequencia();
+  total_freq += freq;
+  _arrel = rinsereix(_arrel, i, s, nom, tlf, arrel, ant, _arrel, freq);
   //cout << "ARREL_AF:" << _arrel->antFr<<endl;
+  numerador += num_puls;
+  //cout<<"numerdador: "<<numerador<<" nom: "<<P.nom()<<endl;
 }
 
-easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string &nom, const nat &telef, bool &arrel, string ant_ant, node_tst* res) throw(error){
+easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string &nom, const nat &telef, bool &arrel, string ant_ant, node_tst* res,const nat &freq) throw(error){
   if(n==NULL){
     node_tst* m = new node_tst;
     m->_esq = m->_dret = m->_central = NULL;
@@ -21,11 +25,13 @@ easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string 
       if(arrel){
         m->maxFr = ant_ant;
         m->antFr = nom;
+        nom="";
         m->_tlf = telef;
         m->_pare = NULL;
+        m->nivell = 0;
         arrel = false;
         i++;
-        m->_central = rinsereix(m->_central, i, s, ant_ant, telef, arrel, ant_ant, m);
+        m->_central = rinsereix(m->_central, i, s, nom, telef, arrel, ant_ant, m, freq);
         /*cout<<"r_inser_arrel_nom:"<<nom<<endl;
 				cout<<"r_inser_arrel_mF:"<<n->maxFr<< "."<<endl;
 				cout<<"r_inser_arrel_aF:"<<n->antFr<< "."<<endl;*/
@@ -37,11 +43,13 @@ easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string 
           m->_tlf = telef;
           ant_ant = m->maxFr;
 					m->_pare = res;
+          m->nivell = i+1;
+          if (m->maxFr != "") num_puls = freq * m->nivell;
           nom = "";
           i++;
           //cout<<"el pare de "<<m->_lletra<<" de maxFr: "<<m->maxFr<<" es: ";
 					//if (res!=NULL) cout<<res->_lletra<<" ."<<endl;
-          m->_central = rinsereix(m->_central, i, s, nom, telef, arrel, ant_ant, m);
+          m->_central = rinsereix(m->_central, i, s, nom, telef, arrel, ant_ant, m, freq);
           /*cout<<"r_inser_nom:"<<nom<<endl;
 					cout<<"r_inser_mF:"<<n->maxFr<< "."<<endl;
 					cout<<"r_inser_aF:"<<n->antFr<< "."<<endl;*/
@@ -55,17 +63,26 @@ easy_dial::node_tst* easy_dial::rinsereix(node_tst* n, nat &i, string s, string 
     return m;
   }
   else{
-    if (n->_lletra > s[i]) n->_esq = rinsereix(n->_esq, i, s, nom, telef, arrel, n->antFr, n);
-    else if (n->_lletra < s[i]) n->_dret = rinsereix(n->_dret, i, s, nom, telef, arrel, n->antFr, n);		
+    if (n->_lletra > s[i]) {
+      n->_esq = rinsereix(n->_esq, i, s, nom, telef, arrel, n->antFr, n, freq);
+    }
+    else if (n->_lletra < s[i]){
+      n->_dret = rinsereix(n->_dret, i, s, nom, telef, arrel, n->antFr, n, freq);		
+    }
     else {
       if(n->maxFr == ""){
+        if (n == _arrel) n->nivell = 1;
         n->maxFr = nom;
         n->_tlf = telef;
         nom = "";
+        if (n->antFr != ""){
+          num_puls = freq * n->nivell;
+          //cout<<num_puls<<" = "<<freq<<" * "<<n->nivell<<endl;
+        }
       }
       ant_ant = n->maxFr;
       i++;
-      n->_central = rinsereix(n->_central, i, s, nom, telef, arrel, ant_ant, n);     // (n->_lletra == s[i])
+      n->_central = rinsereix(n->_central, i, s, nom, telef, arrel, ant_ant, n, freq);     // (n->_lletra == s[i])
     }
     return n;
   }
@@ -194,6 +211,9 @@ easy_dial::easy_dial(const call_registry& R) throw(error){
   R.dump(v);
   bool arr = true;
   _arrel = NULL;
+  total_freq = 0.0;
+  num_puls = 0.0;
+  numerador = 0;
 
   if(v.size() > 0){
     //ordenar vector per frequencia mes alta a mes baixa, he triat mergesort
@@ -348,6 +368,8 @@ void easy_dial::comencen(const string& pref, vector<string>& result) const throw
 }
 
 double easy_dial::longitud_mitjana() const throw(){
-  double res = 0.0;
+  double res = 0;
+  if (total_freq != 0) res = double(numerador) / double(total_freq);
+  //cout<<res<<" = "<<numerador<<" / "<<total_freq<<endl;
   return res;
 }
