66e58e34700ff9c5425ff678f0fa51e0b93104f9
matteogav <45036430+matteogav@users.noreply.github.com>
2020-01-05 20:32:43 +0100

    Provant dialog

--- dialog.cpp
+++ dialog.cpp
@@ -6,22 +6,27 @@ void dialog::dialog(easy_dial& easy, const string& input, vector<string>& answer
   nat num_tlf = 0;       // o num_tlf = easy.num_telf();
   string nom;
   easy.inici();
-  while(i < input.length()-1 and not fi){
+  while(i < input.length() and not fi){
+    cout << "LLETRA: " << input[i] <<endl;
     try{
-      if(input[i] == phone::ENDCHAR){    //??
+      if(input[i] == phone::ENDCHAR){
+				cout << "aqui1" <<endl;
         num_tlf = easy.num_telf();
         fi = true;
         i++;
       }
       else if(input[i] == phone::DELETECHAR){
+        cout << "aqui2" <<endl;
         nom = easy.anterior();
         num_tlf = easy.num_telf();
         i++;
       }
-      else{                               //Ni la variable 'i' ni 'num_tlf' es poden factoritzar i treure fora,
-        num_tlf = easy.num_telf();        //segueixen un ordre de la manera que se li dona el valor segons l'opcio
-        i++;                              //encara que aixo un 'prototip' del que crec que podria ser la solucio
+      else{              
+				cout << "aqui3" <<endl;                
+        num_tlf = easy.num_telf();       
+        i++;                              
         nom = easy.seguent(input[i]);
+				cout << "nom:"<<nom<<endl;
       }
       answers.push_back(nom);
     }
