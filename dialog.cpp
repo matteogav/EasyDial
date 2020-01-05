4bbab3c86a319a1553da397625294d4be0f46c9c
matteogav <matteogav@gmail.com>
2020-01-05 18:55:19 +0100

    Crec que he afegit un joc de prova de dialog

--- dialog.cpp
+++ dialog.cpp
@@ -4,23 +4,32 @@ void dialog::dialog(easy_dial& easy, const string& input, vector<string>& answer
   nat i = 0;
   bool fi = false;
   nat num_tlf = 0;       // o num_tlf = easy.num_telf();
-
+  string nom;
   easy.inici();
   while(i < input.length()-1 and not fi){
-    if(input[i] == phone::ENDCHAR){    //??
-      num_tlf = easy.num_telf();
+    try{
+      if(input[i] == phone::ENDCHAR){    //??
+        num_tlf = easy.num_telf();
+        fi = true;
+        i++;
+      }
+      else if(input[i] == phone::DELETECHAR){
+        nom = easy.anterior();
+        num_tlf = easy.num_telf();
+        i++;
+      }
+      else{                               //Ni la variable 'i' ni 'num_tlf' es poden factoritzar i treure fora,
+        num_tlf = easy.num_telf();        //segueixen un ordre de la manera que se li dona el valor segons l'opcio
+        i++;                              //encara que aixo un 'prototip' del que crec que podria ser la solucio
+        nom = easy.seguent(input[i]);
+      }
+      answers.push_back(nom);
+    }
+    catch(error e){
       fi = true;
-      i++;
-    }
-    else if(input[i] == phone::DELETECHAR){
-      easy.anterior();
-      num_tlf = easy.num_telf();
-      i++;
-    }
-    else{                               //Ni la variable 'i' ni 'num_tlf' es poden factoritzar i treure fora,
-      num_tlf = easy.num_telf();        //segueixen un ordre de la manera que se li dona el valor segons l'opcio
-      i++;                              //encara que aixo un 'prototip' del que crec que podria ser la solucio
-      easy.seguent(input[i]);
+      num_tlf = 0;
+      answers.push_back(e.mensaje());
     }
   }
+  numtelf = num_tlf;
 }
