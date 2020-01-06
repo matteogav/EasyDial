f701f5c484790e32d26b6d34138c8ab63ec86bcb
matteogav <45036430+matteogav@users.noreply.github.com>
2020-01-06 11:43:27 +0100

    Dialog "funcionant"

--- dialog.cpp
+++ dialog.cpp
@@ -3,32 +3,26 @@
 void dialog::dialog(easy_dial& easy, const string& input, vector<string>& answers, nat& numtelf) throw(){
   nat i = 0;
   bool fi = false;
-  nat num_tlf = 0;       // o num_tlf = easy.num_telf();
+  nat num_tlf = 0;
   string nom;
   easy.inici();
-	cout << "PUTO NOM:" << easy.seguent(input[i]) << endl;
 	
   while(i < input.length() and not fi){
-    cout << "LLETRA: " << input[i] <<endl;
     try{
       if(input[i] == phone::ENDCHAR){
-				cout << "aqui" <<endl;
+				nom = easy.seguent(phone::ENDPREF);
         num_tlf = easy.num_telf();
         fi = true;
-        i++;
       }
       else if(input[i] == phone::DELETECHAR){
-        cout << "aqui2" <<endl;
         nom = easy.anterior();
         num_tlf = easy.num_telf();
         i++;
       }
-      else{              
-				cout << "aqui3" <<endl;                
-        num_tlf = easy.num_telf();       
-        i++;                              
+      else{                                                               
         nom = easy.seguent(input[i]);
-				cout << "nom:"<<nom<<endl;
+        num_tlf = easy.num_telf();
+        ++i;
       }
       answers.push_back(nom);
     }
